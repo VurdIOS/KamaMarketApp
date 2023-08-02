f0d4461b2961b7cb0deb8117bff579799efddfeb
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-08-02 17:06:27 +0300

    make the logo with animation

--- KamaMarketApp/Authorization/AuthorizationViewController.swift
+++ KamaMarketApp/Authorization/AuthorizationViewController.swift
@@ -9,24 +9,42 @@ import UIKit
 
 class AuthorizationViewController: UIViewController {
     
-    var logoView = UIImageView()
+    lazy var logoView: UIImageView = {
+        let logo = UIImageView()
+        logo.frame.size.width = view.frame.width - 40
+        logo.frame.size.height = logo.frame.width * 0.127
+        logo.center = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+        //
+        return logo
+    }()
     var logoImage: UIImage!
+    var heightAnc = NSLayoutConstraint()
     
     private var viewModel: AuthorizationViewModelProtocol! {
         didSet {
             logoImage = UIImage(named: viewModel.logoImage)
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray5
         viewModel = AuthorizationViewModel()
+        
         setupLogo()
+        setupConstraints()
+        updateConstraint()
+    }
+    
+    func setupConstraints() {
         view.addSubview(logoView)
-        logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        logoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        logoView.widthAnchor.constraint(equalToConstant: view.frame.width - 300).isActive = true
+    }
+    
+    func updateConstraint() {
+        UIView.animate(withDuration: 1,delay: 0.5,options: .curveEaseInOut ) { [self] in
+            self.logoView.center = CGPoint(x: view.frame.width / 2, y: 150)
+            self.view.layoutIfNeeded() // no needed
+        }
     }
     
     func setupLogo() {
@@ -34,7 +52,5 @@ class AuthorizationViewController: UIViewController {
         logoView.clipsToBounds = true
         logoView.translatesAutoresizingMaskIntoConstraints = false
     }
-
-
 }
 
